91dfadaa4cf1c72f5e0c311ce6b9c16f4323c3a6
BISHER AL MASRI <bbgyttv@gmail.com>
2024-12-01 23:21:14 +0400

    Done AOC Day 1 (i did not know there was a part 2)

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,6 +1,49 @@
-use std::fs;
+// part 1
+
+// use std::fs;
+// fn main() {
+//     let (mut leftVector, mut rightVector): (Vec<i32>, Vec<i32>) = fs::read_to_string("src/input.input")
+//         .unwrap()
+//         .lines()
+//         .map(|line| {
+//             let section: Vec<i32> = line
+//             .split_whitespace()
+//             .filter_map(|s| s.parse::<i32>().ok())
+//             .collect();
+//             (section[0], section[1])
+//         }).fold((vec![], vec![]), |(mut left, mut right), (l, r)| {
+//             left.push(l);
+//             right.push(r);
+//             (left, right)
+//         });
+
+//     // debug lines WHY IS RUST SO WIERD
+
+//     // println!("{}", leftVector.len());
+
+//     // println!("{:?}", leftVector);
+//     // println!("{:?}", rightVector);
+
+//     // UPDATE: IM CORRECT LETS OFGOOOOO
+
+//     leftVector.sort();
+//     rightVector.sort();
+
+//     let mut num = 0;
+
+//     for i in 0..leftVector.len() {
+//         num += (leftVector[i] - rightVector[i]).abs();
+//     }
+
+//     println!("{}", num);
+// }
+
+// part 2
+
+use std::{collections::HashMap, fs};
+
 fn main() {
-    let (mut numbers, mut numbers2): (Vec<i32>, Vec<i32>) = fs::read_to_string("src/input.input")
+    let (left_vector, mut right_vector): (Vec<i32>, Vec<i32>) = fs::read_to_string("src/input.input")
         .unwrap()
         .lines()
         .map(|line| {
@@ -14,24 +57,23 @@ fn main() {
             right.push(r);
             (left, right)
         });
+    
 
-    // debug lines WHY IS RUST SO WIERD
+    let mut frequency_map = HashMap::new();
 
-    // println!("{}", numbers.len());
-
-    // println!("{:?}", numbers);
-    // println!("{:?}", numbers2);
-
-    // UPDATE: IM CORRECT LETS OFGOOOOO
-
-    numbers.sort();
-    numbers2.sort();
-
-    let mut num = 0;
-
-    for i in 0..numbers.len() {
-        num += (numbers[i] - numbers2[i]).abs();
+    for &num in &right_vector {
+        *frequency_map.entry(num).or_insert(0) += 1;
     }
 
-    println!("{}", num);
+    let mut sim = 0;
+    for &num in &left_vector {
+        let freq = frequency_map.get(&num).unwrap_or(&0);
+        sim += num * freq;
+    }
+
+    println!("{}", sim);
 }
+
+
+
+
